0bf511a3b00867a013510e92925dba9247443211
Jerome Noiret <jeromenoiret@free.fr>
2020-07-30 12:59:19 +0200

    API operational without the like dislike system

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -7,7 +7,7 @@ const sauceSchema = mongoose.Schema({
   mainPepper: { type: String, required: true }, // principal ingrédient dans la sauce
   imageUrl: { type: String, required: true }, // string de l'image de la sauce téléchargée par l'utilisateur
   heat: { type: Number, min: 1, max: 10, default: 1 }, // nombre entre 1 et 10 décrivant la sauce
-  userId: { type: String, unique: true, required: true }, //identifiant unique MongoDB pour l'utilisateur qui a créé la sauce
+  userId: { type: String, required: true }, //identifiant unique MongoDB pour l'utilisateur qui a créé la sauce
   likes: { type: Number, default: 0}, // nombre d'utilisateurs qui aiment la sauce
   dislikes: { type: Number, default: 0}, // nombre d'utilisateurs qui n'aiment pas la sauce
   userLiked: [{ type: String }], // tableau d'identifiants d'utilisateurs ayant aimé la sauce
